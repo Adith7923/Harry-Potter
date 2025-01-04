1f63d4c8889e54f54d992ae46f98c18200db9eaf
Adith7923 <adith7923@gmail.com>
2025-01-04 14:55:57 +0530

    chore : hovering color difference added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const darkModeButton = document.getElementById('darkMode');
 darkModeButton.addEventListener("click", () => {
+    console.log("Working")
     document.body.classList.toggle('dark-mode');
     darkModeButton.innerText =
         document.body.classList.contains('dark-mode') ? "Lumos" : "Nox";
